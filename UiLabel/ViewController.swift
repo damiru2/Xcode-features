d34e7b68ae9db9cc25169f4550ebb354d408239c
damiru2 <chalkarov11@mail.ru>
2022-07-14 22:23:43 +0500

    add new button by code

--- UiLabel/ViewController.swift
+++ UiLabel/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
-  override func viewDidLoad() {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    override func viewDidLoad() {
         super.viewDidLoad()
         
+        trueButton.layer.borderColor = UIColor.red.cgColor
+        trueButton.layer.borderWidth = 1
+        trueButton.layer.cornerRadius = 10
+        
+        let actionButton = UIButton()
+        view.addSubview(actionButton)
+        
+        actionButton.layer.borderColor = UIColor.red.cgColor
+        actionButton.layer.borderWidth = 1
+        actionButton.layer.cornerRadius = 10
+        
+        actionButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -153),
+            actionButton.widthAnchor.constraint(equalToConstant: 145),
+            actionButton.heightAnchor.constraint(equalToConstant: 109)])
+        
+        actionButton.setTitle("Действие", for: .normal)
+        actionButton.setTitleColor(.red, for: .normal)
+        actionButton.setTitleColor(.orange, for: .highlighted)
+        actionButton.showsTouchWhenHighlighted = true
+        
+        actionButton.addTarget(self, action: #selector(letAction), for: .touchUpInside)
+        
 }
-
+    
+    @IBAction func trueAction(_ sender: UIButton) {
+        
+        
+    }
+    
+    @objc func letAction() {
+        titleLabel.text = ""
+    }
     
 }
 
