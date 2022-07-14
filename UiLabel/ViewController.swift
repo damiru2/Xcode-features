e63b0ca604dba0b6a14a44b4e1909213d2ed43ca
damiru2 <chalkarov11@mail.ru>
2022-07-14 21:02:22 +0500

    add new labels by soft and by code

--- UiLabel/ViewController.swift
+++ UiLabel/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    let codeLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(codeLabel)
+        
+        codeLabel.frame = CGRect(x: 100, y: 300, width: 200, height: 150)
+        codeLabel.text = "Some text for codeLabel generated from code"
+        codeLabel.textColor = .red
+        codeLabel.numberOfLines = 0
+        
+        codeLabel.shadowColor = .blue
+        codeLabel.shadowOffset = CGSize(width: 1, height: 1)
+        
+        codeLabel.layer.cornerRadius = 5
+        codeLabel.layer.borderColor = UIColor.red.cgColor
+        codeLabel.layer.borderWidth = 2
+        
     }
 
 
